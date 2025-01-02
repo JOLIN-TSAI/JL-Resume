67bfbaf0f366fcd59b54b8dd74f66cb90f3371b3
Jolin <lonely507487@gmail.com>
2025-01-02 14:09:54 +0800

    refactor: ExperienceTimeline 修復結構、切換背景時的顏色

--- src/components/ExperienceTimeline/ExperienceTimeline.tsx
+++ src/components/ExperienceTimeline/ExperienceTimeline.tsx
@@ -4,56 +4,58 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { FaBriefcase, FaGraduationCap, FaStar } from "react-icons/fa";
-
-const timelineData = [
-	{
-		date: "2018 - 2022",
-		title: "實踐大學畢業",
-		subtitle: "觀光管理學系",
-		description: "完成學士學位，專注於觀光管理與相關專業知識。",
-		icon: <FaGraduationCap />,
-		background: "rgb(33, 150, 243)",
-	},
-	{
-		date: "Dec 2022 - Feb 2023",
-		title: "誠品股份有限公司",
-		subtitle: "文具禮品人員",
-		description: "協助店內文具與禮品銷售、商品陳列，並提供顧客服務。",
-		icon: <FaBriefcase />,
-		background: "rgb(33, 150, 243)",
-	},
-	{
-		date: "Feb 2023 - Apr 2023",
-		title: "承勁實業有限公司 (mygreat)",
-		subtitle: "工程專案秘書",
-		description:
-			"協助專案進度跟蹤、會議紀錄整理，並協調內外部溝通以確保專案順利進行。",
-		icon: <FaBriefcase />,
-		background: "rgb(33, 150, 243)",
-	},
-	{
-		date: "Sep 2023 - May 2024",
-		title: "志光教育科技股份有限公司",
-		subtitle: "數位行銷企劃",
-		description:
-			"負責數位廣告投放與效益分析、社群經營，並策劃線上活動以提升品牌曝光。",
-		icon: <FaBriefcase />,
-		background: "rgb(33, 150, 243)",
-	},
-	{
-		date: "Jun 2024 - Nov 2024",
-		title: "資展國際股份有限公司（原資策會）",
-		subtitle: "前端工程師課程學員",
-		description:
-			"學習 HTML、CSS、JavaScript、React 框架以及 RESTful API 開發。",
-		icon: <FaGraduationCap />,
-		background: "rgb(233, 30, 99)",
-	},
-];
+import { useBackgroundState } from "@/hooks/backgroundState";
 
 export default function Timeline() {
+	const { isDarkMode } = useBackgroundState();
+	const timelineData = [
+		{
+			date: "2018 - 2022",
+			title: "實踐大學畢業",
+			subtitle: "觀光管理學系",
+			description: "完成學士學位，專注於觀光管理與相關專業知識。",
+			icon: <FaGraduationCap />,
+			background: isDarkMode ? "#94b2d7" : "rgb(85, 167, 234)",
+		},
+		{
+			date: "Dec 2022 - Feb 2023",
+			title: "誠品股份有限公司",
+			subtitle: "文具禮品人員",
+			description:
+				"負責文具禮品銷售、商品陳列與顧客服務，執行促銷活動、庫存管理及收銀作業，提升門市業績與顧客滿意度。",
+			icon: <FaBriefcase />,
+			background: isDarkMode ? "#94b2d7" : "rgb(85, 167, 234)",
+		},
+		{
+			date: "Feb 2023 - Apr 2023",
+			title: "承勁實業有限公司 (mygreat)",
+			subtitle: "工程專案秘書",
+			description:
+				"協協助專案進度追蹤與會議紀錄，負責設計審核、工程報告製作，並與業主及施工團隊溝通，確保工程順利完成。",
+			icon: <FaBriefcase />,
+			background: isDarkMode ? "#94b2d7" : "rgb(85, 167, 234)",
+		},
+		{
+			date: "Sep 2023 - May 2024",
+			title: "志光教育科技股份有限公司",
+			subtitle: "數位行銷企劃",
+			description:
+				"負責數位廣告投放、社群經營與行銷活動策劃，精準分析廣告效益並優化策略，成功提升品牌曝光與課程轉化率。",
+			icon: <FaBriefcase />,
+			background: isDarkMode ? "#94b2d7" : "rgb(85, 167, 234)",
+		},
+		{
+			date: "Jun 2024 - Nov 2024",
+			title: "資展國際股份有限公司（原資策會）",
+			subtitle: "前端工程師課程學員",
+			description:
+				"學習前端開發技術，包含 HTML、CSS、JavaScript、React 及 RESTful API，擔任組長，帶領團隊完成專案開發與管理。",
+			icon: <FaGraduationCap />,
+			background: isDarkMode ? "#E16EB9" : "rgb(235, 69, 125)",
+		},
+	];
 	return (
-		<VerticalTimeline>
+		<VerticalTimeline lineColor={isDarkMode ? "#94b2d7" : "#aaa"}>
 			{timelineData.map((item, index) => (
 				<VerticalTimelineElement
 					key={index}
@@ -61,7 +63,7 @@ export default function Timeline() {
 					date={item.date}
 					contentStyle={{
 						background: item.background,
-						color: "#fff",
+						color: isDarkMode ? "#000" : "#fff",
 					}}
 					contentArrowStyle={{
 						borderRight: `7px solid ${item.background}`,
@@ -78,7 +80,7 @@ export default function Timeline() {
 			))}
 			<VerticalTimelineElement
 				iconStyle={{
-					background: "rgb(16, 204, 82)",
+					background: isDarkMode ? "#4FE98F" : "rgb(16, 204, 82)",
 					color: "#fff",
 				}}
 				icon={<FaStar />}
