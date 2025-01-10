e019819192d3d1135d6409d847a734fe76be3657
Jolin <lonely507487@gmail.com>
2025-01-10 22:40:42 +0800

    feat:add Three.js Book's components

---
+++ src/components/ThreeBook/Content.tsx
@@ -0,0 +1,9 @@
+// import React, { useState, useEffect } from 'react'
+
+export default function Content() {
+	return (
+		<>
+			<div>Content</div>
+		</>
+	);
+}
---
+++ src/components/ThreeBook/Ul.tsx
@@ -0,0 +1,9 @@
+// import React, { useState, useEffect } from 'react'
+
+export default function Ul() {
+	return (
+		<>
+			<div>Ul</div>
+		</>
+	);
+}
